f333d4061b121f96efcf54e0af3bb56fbc4e8203
OrengoA1587 <102257741+OrengoA1587@users.noreply.github.com>
2023-08-18 16:29:46 -0400

    Login Functionality
    -Added login action page to test login functionality.
    -Database pending and will be added soon to test functionality.

---
+++ ChatGPTBlazer/BlazerUI/wwwroot/LoginAction.js
@@ -0,0 +1,16 @@
+﻿
+ 
+ 
+
+
+function loginPage(form) {
+    if (form.username.value == "test") {
+        if (form.password.value == "test") {
+            location = "/LoginTest"
+        }
+        else {
+            alert("Incorrect detail Password")
+        }
+
+    }
+}
