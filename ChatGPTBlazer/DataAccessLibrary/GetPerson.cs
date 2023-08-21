24b1f1441d5679a15689a471bd3ee964dc23c96d
OrengoA1587 <102257741+OrengoA1587@users.noreply.github.com>
2023-08-21 07:15:35 -0400

    LoginModal Update
    -Created functional login for testing purposes only.

--- ChatGPTBlazer/DataAccessLibrary/GetPerson.cs
+++ ChatGPTBlazer/DataAccessLibrary/GetPerson.cs
@@ -1,63 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlTypes;
-using System.Reflection.Metadata;
-using Microsoft.Data.SqlClient;
-using Microsoft.IdentityModel.Protocols;
+﻿using Microsoft.Data.SqlClient;
 using System.Data;
-using NLog.Internal;
+using System;
 using Microsoft.Extensions.Configuration;
-using System.Configuration;
-using System.Xml.Linq;
-
+using Microsoft.IdentityModel.Protocols;
+using System.IO;
 namespace DataAccessLibrary
 {
-    public class GetPerson
+    public class GetPerson  
     {
-        private string _username = "";
-        private string _password = "";
-        private IConfiguration _configuration;
-
-        public GetPerson(string username, string password)
+        public bool VerifyPerson(string username,string password, string connectionString)
         {
-            _username = username;
-            _password = password;
-
-            Console.WriteLine(_username + _password);
-            
-
-        }
-        public static bool VerifyPerson(string username,string password)
-
-        {
-            
             SqlConnection sqlcon = new SqlConnection();
             string statement = "SELECT * FROM PeopleTest where Username = @username AND Password = @password";
-            sqlcon.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PersonDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-
+            //sqlcon.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PersonDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            sqlcon.ConnectionString = connectionString;
 
             using (SqlCommand sqlcom = new SqlCommand(statement, sqlcon))
             {
                 sqlcon.Open();
                 sqlcom.Parameters.Add("@username", SqlDbType.NVarChar).Value = username;
-                sqlcom.Parameters.Add("@password", SqlDbType.NVarChar).Value = password;
-
-
-
+                sqlcom.Parameters.Add("@password", SqlDbType.NVarChar).Value = password;                
                 SqlDataReader reader = sqlcom.ExecuteReader();
                 if (reader.Read())
                 {
                     string userName = reader.GetValue(0).ToString();
                     string passwordCheck = reader.GetValue(1).ToString();
-
                     sqlcon.Close();
                     return true;
-                    
-
                 }
                 else
                 {
@@ -65,21 +34,8 @@ namespace DataAccessLibrary
                     return false;
                 }
                 sqlcon.Close();
-
             }
             return false;
-        }
-        public string Username
-        {
-            get { return _username; }
-            set { _username = value; }
-        }
-        public string Password
-        {
-            get { return _password; }
-            set { _password = value; }
-        }
-
-         
+        }         
     }
 }
